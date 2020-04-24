1b7c50ccc6124f3d4fdd510839f0ce2d4ca26b46
Jonathan <jonathan@twonary.com>
2020-04-24 07:56:49 -0400

    Project Created

---
+++ app/src/main/java/com/jahk/mvvmconceptapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jahk.mvvmconceptapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
